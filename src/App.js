446b4daaa6ce533697bae90319ee7e557dd1a08c
sova0110 <sova011089@gmail.com>
2024-09-10 19:38:48 +0300

    add_react_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React from "react";
+import { Chart } from "react-google-charts";
+
+
+
+export const data = [
+  ["Содержание витамина С", "мг в 100 гр"],
+  ["Шиповник сухой", 1200],
+  ["Черная смородина", 200],
+  ["Апельсин", 60],
+  ["Перец красный", 250],
+  ["Лимон", 40],
+];
+
+export const options = {
+  title: "Содержание витамина С в продуктах",
+};
+
+export const areaChartData = [
+  ["Месяц", "выручка", "себестоимость"],
+  ["январь",  1000, 400],
+  ["февраль",  1170, 460],
+  ["март",  660, 1120],
+  ["апрель",  1030, 540],
+  ["май",  900, 1020],
+  ["июнь",  1300, 540],
+  ["июль",  1003, 504],
+  ["авуст",  1500, 740],
+];
+
+export const areaChartOptions = {
+  title: "Доходы и расходы компании Рога и копыта",
+  hAxis: { title: "месяц", },
+  vAxis: { title: "сумма", },
+  isStacked: true,
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
+      <Chart
+        chartType="AreaChart"
+        data={areaChartData}
+        options={areaChartOptions}
+        width={"100%"}
+        height={"400px"}
+      />
     </div>
   );
 }
 
 export default App;
+
